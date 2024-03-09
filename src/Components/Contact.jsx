e5d2b5ed7e48d004ef7717b3babf99171ea37f22
sasheed <e19271@eng.pdn.ac.lk>
2024-03-10 05:17:58 +0530

    make contact component tablet responsive

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div id='Contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16 '>
+    <div id='Contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16 sm:h-screen sm:pt-20 sm:pr-6'>
         <h1 className='py-4 text-4xl font-bold text-center text-[#001b5e] '>Contact</h1>
-        <form action="https://getform.io/f/paynnmpb" method='POST' encType='multipart/form-data'>
+        <form action="https://getform.io/f/paynnmpb" method='POST' encType='multipart/form-data' className='sm:pt-12'>
           
           <div className='grid w-full gap-4 py-2 md:grid-cols-2 '>
             <div className='flex flex-col '>
@@ -33,9 +33,13 @@ const Contact = () => {
             <textarea className='p-3 border-2 border-gray-300 rounded-lg focus:border-blue-300 focus:outline-none' rows='10' type="email" name="message"  ></textarea>
           </div>
 
-          <button className='bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg'>
+          <div className='sm:pt-14'>
+          <button className='bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg '>
             Send Massage
           </button>
+          </div>
+
+          
 
         </form>
       
