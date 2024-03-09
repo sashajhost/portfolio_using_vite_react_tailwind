576eeb6014e3a0778a630f29ca3a7039875375a1
sasheed <e19271@eng.pdn.ac.lk>
2024-03-10 05:07:21 +0530

    make projects and project items components tablet responsive

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -7,9 +7,9 @@ import property4 from '../assets/property4.jpg';
 
 const Projects = () => {
   return (
-    <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16  '>
-        <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
-        <p className='py-8 text-center '>
+    <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16 sm:h-screen sm:pl-20 sm:pr-10 '>
+        <h1 className='text-4xl font-bold text-center text-[#001b5e] sm:pt-12'>Projects</h1>
+        <p className='py-8 text-center sm:pt-14 '>
             I possess a well-rounded background in IT, marketing, and visual effects. Leveraging my skills, 
             I created 3D animations for educational purposes in 2020 to support AI students with their physics studies. 
             In 2021, I further contributed to the field by publishing the "phy6days" Android application, which provides 
@@ -17,7 +17,7 @@ const Projects = () => {
              having completed projects for international clients through Fiverr. Currently, I hold the position of CTO 
              at Thrima, a company I founded, leading a team of five talented co-workers.
         </p>
-        <div className='grid gap-12 sm:grid-cols-2 '>
+        <div className='grid gap-12 sm:grid-cols-2 sm:pt-12'>
             <ProjectsItem img={property1} title='animations' cat='blender' link="https://www.youtube.com/@thrimana3000/featured"/>
             <ProjectsItem img={property2} title='react js' cat='web developing' link="https://github.com/sasheeddev/react_tailwind_app" />
             <ProjectsItem img={property3} title='adroid' cat='app developing' link="https://play.google.com/store/apps/details?id=io.ionic.prog612534ff61fda147fec2393f&hl=en&gl=US" />
